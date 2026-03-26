37e55b443df42918f815335148a76a6175b65d5b
Terres06 <pedrocoutoterres@gmail.com>
2026-03-26 18:30:49 -0300

    feat: create device filter

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from db.schema import ReadDHT22, Session
 
@@ -5,10 +6,13 @@ app = FastAPI()
 
 @app.get("/sensor-readings")
 
-def get_sensor_readings(limit: int = 100, offset: int = 0):
+def get_sensor_readings(limit: int = 100, offset: int = 0,device_id: Optional[str] = None):
     try:
         db = Session()
-        read = db.query(ReadDHT22).offset(offset).limit(limit).all()
+        query = db.query(ReadDHT22)
+        if device_id:
+            query = query.filter(ReadDHT22.device_id == device_id)
+        read = query.offset(offset).limit(limit).all()
         return [{k: v for k, v in reading.__dict__.items() if k != "_sa_instance_state"} for reading in read]
     except Exception as e:
         return {"error": str(e)}
