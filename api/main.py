42dd4e8f292088a3830128f981b1e9f45a93f5f6
Terres06 <pedrocoutoterres@gmail.com>
2026-03-30 19:57:06 -0300

    refactor: centralize db session management

--- api/main.py
+++ api/main.py
@@ -1,15 +1,21 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from db.schema import ReadDHT22, Session
 
 app = FastAPI()
 
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/sensor-readings")
 
 #endpoint to retrieve all sensor readings with pagination and optional device_id filter
-def get_sensor_readings(limit: int = 100, offset: int = 0,device_id: Optional[str] = None):
+def get_sensor_readings(db = Depends(get_db), limit: int = 100, offset: int = 0, device_id: Optional[str] = None):
     try:
-        db = Session()
         query = db.query(ReadDHT22)
         if device_id:
             query = query.filter(ReadDHT22.device_id == device_id)
@@ -17,15 +23,12 @@ def get_sensor_readings(limit: int = 100, offset: int = 0,device_id: Optional[st
         return [{k: v for k, v in reading.__dict__.items() if k != "_sa_instance_state"} for reading in read]
     except Exception as e:
         return {"error": str(e)}
-    finally:
-        db.close()
 
 @app.get("/last-reading")
 
 #endpoint to retrieve the last sensor reading for a specific device_id or the most recent reading if no device_id is provided
-def get_last_reading(device_id: Optional[str] = None):
+def get_last_reading(db = Depends(get_db), device_id: Optional[str] = None):
     try:
-        db = Session()
         query = db.query(ReadDHT22)
         if device_id:
             query = query.filter(ReadDHT22.device_id == device_id)
@@ -33,5 +36,3 @@ def get_last_reading(device_id: Optional[str] = None):
         return {k: v for k, v in read.__dict__.items() if k != "_sa_instance_state"} if read else {"error": "No readings found for this device"}
     except Exception as e:
         return {"error": str(e)}
-    finally:
-        db.close()
