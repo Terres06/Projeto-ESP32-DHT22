55cd3a8ee9b279c1a9560ce1ee0c73b649e4a389
Terres06 <pedrocoutoterres@gmail.com>
2026-04-16 16:01:55 -0300

    feat: create ESP32 broker publisher and DHT22 reader

---
+++ Firmware/include/config.example.h
@@ -0,0 +1,8 @@
+#define WIFI_SSID "Sua rede"
+#define WIFI_PASSWORD "Sua senha de rede"
+#define MQTT_BROKER "192.168.x.x"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "sensors/dht22"
+#define DEVICE_ID "ESP32_01"
+#define DHT_PIN 4
+#define DHT_TYPE DHT22
---
+++ Firmware/src/main.cpp
@@ -0,0 +1,78 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "config.h"
+#include <ArduinoJson.h>
+#include "DHT.h"
+
+DHT dht(DHT_PIN, DHT_TYPE);
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup(){
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  client.setServer(MQTT_BROKER, MQTT_PORT);
+  dht.begin();
+}
+
+unsigned long lastReconnectAttempt = 0;
+
+bool reconnect(){
+  if (!client.connected()) {
+    unsigned long now = millis();
+    if (now - lastReconnectAttempt > 5000) {
+      lastReconnectAttempt = now;
+      Serial.println("Connecting to MQTT...");
+      if(client.connect(DEVICE_ID)){
+        Serial.println("Connected to MQTT");
+        return true;
+      }else{
+        Serial.print("Failed rc=");
+        Serial.println(client.state());
+        return false;
+      }
+    }else{
+      return false;
+    }
+  }else{
+    return true;
+  }
+} 
+
+void readAndPublish(){
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  JsonDocument doc;
+  doc["humidity"] = humidity;
+  doc["temperature"] = temperature;
+  doc["device_id"] = DEVICE_ID;
+  char jsonBuffer[200];
+  serializeJson(doc, jsonBuffer);
+  
+  if(client.publish(MQTT_TOPIC, jsonBuffer)){
+    Serial.println("Data published to MQTT");
+  }else{
+    Serial.println("Failed to publish data to MQTT");
+  }
+  
+}
+
+void loop(){
+  if(reconnect()){
+    readAndPublish();
+  }
+  client.loop();
+  delay(1500);
+}
